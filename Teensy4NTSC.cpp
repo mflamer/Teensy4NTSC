067f5c5d616d4f5a8ba7b51ae4718c03024c7c70
Mark Flamer <mflamer@users.noreply.github.com>
2021-12-30 13:36:18 -0800

    Test multiple V sync lines vs 1

--- Teensy4NTSC.cpp
+++ Teensy4NTSC.cpp
@@ -83,7 +83,7 @@ void Teensy4NTSC::sendLine(){
       FLEXIO2_SHIFTBUFBIS7 = buffer[line][7];
       FLEXIO2_SHIFTBUFBIS0 = buffer[line][0];             
    }
-   else if(line == V_SYNC){        
+   else if(line >= V_SYNC){        
 
       digitalWriteFast(3, LOW); 
       delayMicroseconds(H_DEAD);
